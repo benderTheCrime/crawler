7f52b1370c9080e25075a685fcf1b9c27e73256e
jgrosecl49 <joe.groseclose@ordergroove.com>
2015-12-06 10:58:28 -0500

    Added new main file

---
+++ src/main/java/core/crawler/Crawler.java
@@ -0,0 +1,41 @@
+package src.main.java.core.crawler;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Crawler {
+    public static void main(String args[])
+    {
+        // TODO The arguments could be many things here, but here are a few 
+        // key players:
+        // - Root URL
+        // - Number of Threads to open max
+        // - Number of Websites to "Crawl to"
+        // - Max time duration
+        // - + Any results you are trying to obtain
+
+        // Pull our URL from the arguments chain
+        // TODO probably a good idea to do some type safety checking here
+        String rootUrl = args[0];
+        URL parsedRootUrl = null;
+        int maxNumberOfThreads = Integer.valueOf(args[1]);
+
+        // Try and declare our new URL
+        try
+        {
+            parsedRootUrl = new URL(rootUrl);
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+
+        // Next, we are going to initialize threads
+        
+    }
+
+    @SuppressWarnings("unused")
+    private Thread initializeThread()
+    {
+        return null;
+    }
+}
--- src/main/java/wc/core/api/App.java
+++
@@ -1,6 +0,0 @@
-package wc.core.api;
-
-public class App {
-	public static void main(String[] args) {
-	}
-}
